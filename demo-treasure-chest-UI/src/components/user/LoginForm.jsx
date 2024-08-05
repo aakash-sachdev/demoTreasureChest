d2ed97ce56430c76459aae68248bff4059c15115
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-08-05 18:55:31 -0500

    Auth3 created

--- demo-treasure-chest-UI/src/components/user/LoginForm.jsx
+++ demo-treasure-chest-UI/src/components/user/LoginForm.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function LoginForm() {
+
+
   return (
     <>
 
--- demo-treasure-chest-UI/src/services/UserServices.js
+++ demo-treasure-chest-UI/src/services/UserServices.js
@@ -1,9 +1,68 @@
+// import axios from "axios";
+
+// const BASEAPIURL = "http://localhost:8080/api/user";
+
+// //For authorized admin
+// //Will refactor for better security later
+// export const fetchUsers = async () => {
+//   try {
+//     const response = await axios.get(`${BASEAPIURL}/all`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("There was an error fetching all Users!", error);
+//     throw error;
+//   }
+// };
+
+// // export const addUser = async (username, email, password) => {
+// //   try {
+// //     const response = await axios.post(`${BASEAPIURL}/registration`, null, {
+// //       params: { username, password, email },
+// //     });
+// //     return response.data;
+// //   } catch (error) {
+// //     console.error("There was an error creating the User!", error);
+// //     throw error;
+// //   }
+// // };
+// export const addUser = async (username, email, password) => {
+//   try {
+//     const response = await axios.post(
+//       `${BASEAPIURL}/registration`,
+//       {
+//         username,
+//         email,
+//         pwHash: password
+//       },
+//       {
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+//       }
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.error("There was an error creating the User!", error);
+//     throw error;
+//   }
+// };
+
+// export const deleteUser = async (userId) => {
+//   try {
+//     await axios.post(`${BASEAPIURL}/delete`, null, {
+//       params: { userId },
+//     });
+//   } catch (error) {
+//     console.error("There was an error deleting the User!", error);
+//     throw error;
+//   }
+// };
+
 import axios from "axios";
 
-const BASEAPIURL = "http://localhost:8080/api/user";
+const BASEAPIURL = "http://localhost:8080";
 
-//For authorized admin
-//Will refactor for better security later
+
 export const fetchUsers = async () => {
   try {
     const response = await axios.get(`${BASEAPIURL}/all`);
@@ -14,35 +73,32 @@ export const fetchUsers = async () => {
   }
 };
 
-// export const addUser = async (username, email, password) => {
-//   try {
-//     const response = await axios.post(`${BASEAPIURL}/registration`, null, {
-//       params: { username, password, email },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error("There was an error creating the User!", error);
-//     throw error;
-//   }
-// };
 export const addUser = async (username, email, password) => {
+  const userData = {
+    username,
+    email,
+    verifyEmail: email,
+    password,
+    verifyPassword: password
+  };
+
   try {
-    const response = await axios.post(
-      `${BASEAPIURL}/registration`,
-      {
-        username,
-        email,
-        pwHash: password
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-    );
+    const response = await axios.post(`${BASEAPIURL}/registration`, userData, {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true
+    });
+    console.log(response.data, response.status, response.data.token);
     return response.data;
   } catch (error) {
-    console.error("There was an error creating the User!", error);
+    const errorData = error.response.data;
+    let allDefaultMessages = [];
+
+  
+    for (let i = 0; i < errorData.length; i++) {  
+      allDefaultMessages.push(errorData[i].defaultMessage);
+    }
+
+    console.error("Error registering new user!", allDefaultMessages);
     throw error;
   }
 };
@@ -55,5 +111,25 @@ export const deleteUser = async (userId) => {
   } catch (error) {
     console.error("There was an error deleting the User!", error);
     throw error;
+  }
+};
+
+export const loginUser = async (username, password) => {
+  const loginData = {
+    username,
+    password
+  };
+
+  try {
+    const response = await axios.post(`${BASEAPIURL}/login`, loginData, {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    const errorMessage = handleError(error);
+    console.error("Error logging in user!", errorMessage);
+    throw new Error(errorMessage);
   }
 };
